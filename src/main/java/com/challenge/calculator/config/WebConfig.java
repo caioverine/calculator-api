0bc4044e46e7e39d31162ad3c6a18353a489e533
Caio Vinícius <viniciuscaio.17@gmail.com>
2025-02-22 12:00:48 -0300

    CReation of WebConfig

---
+++ src/main/java/com/challenge/calculator/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.challenge.calculator.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Permite todas as rotas
+                .allowedOrigins("http://localhost:4200") // Permite a origem do seu front-end
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                .allowedHeaders("*") // Permite todos os cabeçalhos
+                .allowCredentials(true); // Permite credenciais (como cookies)
+    }
+}
